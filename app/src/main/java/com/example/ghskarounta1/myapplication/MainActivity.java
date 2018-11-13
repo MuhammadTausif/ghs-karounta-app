931f59adf0bdc940ab0b20a659f75dd461cb33e1
MuhammadTausif <tausifasia@gmail.com>
2018-11-13 13:49:42 +0500

    Singup Activity Added.

--- app/src/main/java/com/example/ghskarounta1/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ghskarounta1/myapplication/MainActivity.java
@@ -6,6 +6,11 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,6 +44,13 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Allah is the God");
+
+
     }
 
 }
